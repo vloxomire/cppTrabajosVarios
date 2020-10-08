78ea498156ad462455dbc7ad9756034006ebdda7
Max Sebastián Saldaña <vloxomire@gmail.com>
2020-10-07 21:50:28 -0300

    falta dibujo
    Corregir ahorcado

--- Ahorcado/Ahorcado.cpp
+++ Ahorcado/Ahorcado.cpp
@@ -3,16 +3,7 @@
 struct EstAhorcado
 {
 	char palabraABuscar[5]{ 'e','m','i','l','y' };
-	char arrayRespuesta[5]{'_'};
-	std::string dibujoAhorcado[6]
-	{
-	"	╔--╗   ",
-	"	|  @   ",
-	"	| /|\ ",
-	"	| / \  ",
-	"	|	   ",
-	"		--------"
-	};
+	char arrayRespuesta[5]{ '_','_','_','_','_' };
 	std::string letrasElegidas{ " " };
 	char letra{ ' ' };
 };
@@ -30,14 +21,52 @@ int main()
 };
 void imprimirPantalla(EstAhorcado& eA)
 {
-	bool ganar(false);
+	std::string dibujoAhorcado[6]
+	{
+	"\n	|	 @ ",
+	"	|    | ",
+	"	|   /| ",
+	"	|   /|\\",
+	"	|    /  ",
+	"	|     \\"
+	};
+	std::string dibujoHorca[6]
+	{
+	"	╔--╗",
+	"	|",
+	"	|",
+	"	|",
+	"	|",
+	"	--------"
+	};
+	bool gano(false);
+	bool letraValida(false);
 	short intentos{ 0 };
+	short victoryPoint(0);
 	do
 	{
+
 		//DIBUJO
+		/*for (size_t i = 0; i < 6; i++)
+		{
+			if (intentos < i)
+			{
+				std::cout << dibujoHorca[i] << '\n';
+			}
+			else
+			{
+				std::cout << dibujoAhorcado[i] << '\n';
+			}
+		}*/
 		for (size_t i = 0; i < 6; i++)
 		{
-			std::cout << eA.dibujoAhorcado[i] << '\n';
+			std::cout << dibujoHorca[i];
+			if (intentos<i)
+			{
+				dibujoHorca[i]= dibujoAhorcado[i];
+			}
+			
+			std::cout << '\n';
 		}
 
 
@@ -45,27 +74,53 @@ void imprimirPantalla(EstAhorcado& eA)
 		std::cout << "Letras Elegidas: " << eA.letrasElegidas << '\n';
 
 		std::cout << "La palabra es: ";
-		/*for (size_t i = 0; i < 5; i++)
+
+
+		for (size_t k = 0; k < 5; k++)
+		{
+			std::cout << eA.arrayRespuesta[k];
+			std::cout << ' ';
+		}
+		
+		std::cout << "\nIngrese una letra: ";
+		std::cin >> eA.letra;
+
+
+		eA.letrasElegidas += eA.letra;
+
+		for (size_t i = 0; i < 5; i++)
 		{
 			if (eA.palabraABuscar[i] == eA.letra)
 			{
 				eA.arrayRespuesta[i] = eA.palabraABuscar[i];
+				letraValida = true;
+				victoryPoint++;
 			}
-		}*/
-		for (size_t k = 0; k < 5; k++)
+		}
+		if (!letraValida)
 		{
-			std::cout << eA.arrayRespuesta[k];
+			intentos++;
+		}
+		else
+		{
+			letraValida = false;
+		}
+		//VIICTORIA
+		if (victoryPoint == 5)
+		{
+			gano = true;
+			std::cout << "Has ganado la partida\n";
+			std::cout << "La palabra era: \n";
+			for (size_t i = 0; i < 5; i++)
+			{
+				std::cout<<eA.palabraABuscar[i];
+			}
+			std::cin.get();
+			std::cin.get();
 		}
 
-
-		std::cout << "\nIngrese una letra: ";
-		std::cin >> eA.letra;
-
-		eA.letrasElegidas += eA.letra;
-
-		intentos++;
 		system("cls");
-	} while (!ganar);
+	} while (!gano);
 
 
 }
