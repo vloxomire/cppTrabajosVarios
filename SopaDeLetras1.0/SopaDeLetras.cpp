13ace730f72e8cf98f5f9b0cf41b2c317940d183
Max "Destroyer" Sebastian Saldaña <40814622+vloxomire@users.noreply.github.com>
2020-10-14 15:18:43 -0300

    func. Converti
    Paso valores a char

--- SopaDeLetras1.0/SopaDeLetras.cpp
+++ SopaDeLetras1.0/SopaDeLetras.cpp
@@ -7,21 +7,24 @@ private:
 	const static short indice{ 10 };
 	char matrizSopa[indice][indice];
 	short matrizNumerosGuia[indice][indice];
-	
-public:
-	SopaDeLetras();
-	~SopaDeLetras();
+	bool salir;
 	void Init();
 	void VerSopa();
 	void VerNumeros();
-	void CrearMatrizSopa();
+	void CrearSopa();
 	void CrearNumeros();
+	void Menu();
+
+public:
+	SopaDeLetras();
+	~SopaDeLetras();
+	void Juego();
 
 };
 int main()
 {
 	SopaDeLetras sp;
-	sp.VerSopa();
+	sp.Juego();
 	return 0;
 };
 
@@ -29,36 +32,58 @@ SopaDeLetras::SopaDeLetras()
 {
 	std::cout << "Construyendo\n";
 	srand(time(0));
-	Init();
 	CrearNumeros();
+	CrearSopa();
+	Init();
+	
 };
 
 SopaDeLetras::~SopaDeLetras() {};
 
 void SopaDeLetras::Init()
 {
-	
+	salir = false;
 };
 
-void SopaDeLetras::VerSopa()
+void SopaDeLetras::Juego()
+{
+
+	Menu();
+
+
+};
+
+void SopaDeLetras::Menu()
 {
 	std::cout << "		SOPA DE LETRAS\n";
-	
-	
-	VerNumeros();
+	std::cout << '\n';
 	std::cout << "F2:Guardar	||F3:Cargar		||	F10 or Esc:	Salir\n";
+	std::cout << '\n';
+	VerSopa();
+	std::cout << '\n';
 	std::cout << "Presiones las flechas para mover el cursor.\n";
 	std::cout << "Presiones 'Espacio' para seleccionar una letra.\n";
 };
 
 void SopaDeLetras::CrearNumeros()
 {
-	
+
 	for (size_t f = 0; f < indice; f++)
 	{
 		for (size_t c = 0; c < indice; c++)
 		{
-			matrizNumerosGuia[f][c] = 65+ rand()% (90 - 65)+1;
+			matrizNumerosGuia[f][c] = 65 + rand() % (90 - 65) + 1;
+		}
+	}
+};
+
+void SopaDeLetras::CrearSopa()
+{
+	for (size_t f = 0; f < indice; f++)
+	{
+		for (size_t c = 0; c < indice; c++)
+		{
+			matrizSopa[f][c] = matrizNumerosGuia[f][c];
 		}
 	}
 };
@@ -69,8 +94,21 @@ void SopaDeLetras::VerNumeros()
 	{
 		for (size_t c = 0; c < indice; c++)
 		{
-			std::cout<<matrizNumerosGuia[f][c]<<' ';
+			std::cout << matrizNumerosGuia[f][c] << ' ';
 		}
-		std::cout <<'\n';
+		std::cout << '\n';
 	}
 };
+
+void SopaDeLetras::VerSopa()
+{
+	for (size_t f = 0; f < indice; f++)
+	{
+		for (size_t c = 0; c < indice; c++)
+		{
+			std::cout << matrizSopa[f][c] << ' ';
+		}
+		std::cout << '\n';
+	}
+};
+
